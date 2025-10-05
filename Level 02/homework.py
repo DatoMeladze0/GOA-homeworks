1a3233e1645f9b5d4706d3b521e09124dc44e9ac
DatoMeladze0 <dmeladze6@gmail.com>
2025-10-05 16:19:03 +0400

    third commit

---
+++ Level 02/homework.py
@@ -0,0 +1,25 @@
+#დეკლარაცია არის ცვლადის შექმნა
+#ინიციალიზაცია არის ცვლადისთვის მნიშვნელობის მინიჭება
+#რეინიციალიზაცია არის ცვლადის მნიშვნელობის განახლება (ახალი მნიშვნელობის მინიჭება)
+
+#1)
+number = 4
+print(number)
+number = 3
+print(number)
+number = 6
+print(number)
+
+#2)
+num1 = 4
+num2 = 5
+num3 = 7
+num4 = 2
+num5 = 4
+print(num1 + num2 + num3 + num4 + num5)
+
+#3)
+length = 20
+width = 10
+Perimeter = length + width
+print(Perimeter)
