f4370cd15527f15944825b9fd11cfdabfe0b019e
DatoMeladze0 <dmeladze6@gmail.com>
2025-10-22 19:49:38 +0400

    initial commit

---
+++ Level 06/Homework.py
@@ -0,0 +1,22 @@
+# 1)
+
+# input-ი არის ინფორმაცია, რომელიც შედის კომპიუტერში. მაგალითად კლავიატურის ღილაკებზე დაჭერა.
+# output-ი არის ინფორმაცია, რომელიც გამოდის კომპიუტერიდან. მაგალითად პრინტერიდან სურათის ამობეჭდვა.
+
+# 2)
+
+number1 = input("enter the first number: ")
+number2 = input("enter the second number: ")
+print(number1 + number2)
+
+# 3)
+
+num1 = int(input("first number: "))
+num2 = int(input("second number: "))
+num3 = int(input("third number: "))
+num4 = int(input("fourth number: "))
+num5 = int(input("fifth number: "))
+
+sum = num1 + num2 + num3 + num4 + num5
+average_arithmetic = sum / 2
+print(average_arithmetic)
---
+++ Level 06/classwork.py
@@ -0,0 +1,9 @@
+#1)
+name = input("enter your name here: ")
+print(name)
+
+#2)
+num1 = int(input("first number: "))
+num2 = int(input("second number: "))
+print(num1 + num2)
+#რიცხვთა ჯამი ვერ მივიღეთ, რადგან კომპიუტერი ამ შეყვანილ ციფრებს აღიქვამს string-ად ანუ ტექსტურ მონაცემად და არა integer-ად.
