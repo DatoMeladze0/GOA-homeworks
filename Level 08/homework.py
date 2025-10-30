a9c50a4eb77451c93bff5481170dbb5f672163a9
DatoMeladze0 <dmeladze6@gmail.com>
2025-10-30 19:08:53 +0400

    commit

---
+++ Level 08/homework.py
@@ -0,0 +1,36 @@
+# 1)
+
+#type() არის ფუნქცია, რომელიც ამოწმებს მისთვის გადაცემული მონაცემის ტიპს.
+#Data conversion არის ცვლადის მნიშვნელობის მონაცემის ტიპის შეცვლა, შესაბამისი ფუნქციების გამოყენებით. მაგალითად: float(), str(), bool(), int().
+
+# 2)
+
+# num = int("1")
+# num1 = int("5")
+# num3 = int ("3")
+
+# num4 = float("5")
+# num5 = float("2")
+# num6 = float("8")
+
+# num7 = str(5)
+# num8 = str(14)
+# num9 = str(9)
+
+# num10 = bool(1)
+# num11 = bool(0)
+# num12 = bool(2)
+
+# 3)
+
+# number1 = int(input())
+# number2 = int(input())
+# number3 = int(input())
+# print(number1 + number2 + number3)
+# print(str(number1) + str(number2) + str(number3))
+
+# 4)
+
+# name = str(input())
+# number = int(input())
+# print(name * number)
