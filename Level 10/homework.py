90edc87d895ae23d873d9fcad2cf7e77ad57615f
DatoMeladze0 <dmeladze6@gmail.com>
2025-11-06 16:25:50 +0400

    initial commit

---
+++ Level 10/homework.py
@@ -0,0 +1,73 @@
+# 1)
+# შედარების ოპერატორები: >, <, >=, <=, ==, !=
+# print(5 > 4)
+# print(3 > 2)
+# print(6 > 1)
+# print(8 > 2)
+# print(10 > 9)
+
+# print(43 < 52)
+# print(23 < 94)
+# print(23 < 49)
+# print(85 < 87)
+# print(69 < 73)
+
+# print(6 >= 6)
+# print(9 >= 8)
+# print(12 >= 5)
+# print(90 >= 9)
+# print(45 >= 23)
+
+# print(12 <= 44)
+# print(54 <= 90)
+# print(3 <= 4)
+# print(4 <= 4)
+# print(1 <= 23)
+
+# print(9 == 9)
+# print(23 == 23)
+# print(2 == 2)
+# print(43 == 43)
+# print(76 == 76)
+
+# print(4 != 4)
+# print(5 != 5)
+# print(80 != 80)
+# print(54 != 54)
+# print(86 != 86)
+
+# 2)
+# ლოგიკური ოპერატორები: and, or
+
+# print(True and True) # True
+# print(True and False) # False
+# print(False and True) # False
+# print(False and False) # False
+
+# print(True or True) # True
+# print(True or False) # True
+# print(False or True) # True
+# print(False or False) # False
+
+# 3)
+# print((3 > 2) and (4 < 6))
+# print((8 == 8) and (1 > 0))
+# print((6 < 7) and (90 > 5))
+
+# print((6 > 9) or (7 < 12))
+# print((16 > 3) or (12 > 11))
+# print((74 > 23) or (24 < 2))
+
+# 4)
+# number = 10
+# number2 = int(input())
+# print(number < number2)
+
+# 5)
+# my_name = "Dato"
+# username = input()
+# print(my_name == username)
+
+# 6)
+# user_age = int(input())
+# print(user_age > 18)
