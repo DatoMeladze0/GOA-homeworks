b82f95cee6c954aea651e3c42ed3de51d1bfaa1e
DatoMeladze0 <dmeladze6@gmail.com>
2025-11-20 15:22:06 +0400

    initial commit

---
+++ Level 14/classwork.py
@@ -0,0 +1,16 @@
+#1)
+
+# for i in range(100):
+#     print(i)
+
+#2)
+
+# for i in range(101):
+#     print(i)
+
+#3)
+
+for i in range(5):
+    number = int(input())
+    kvadratshi = number * number
+    print(kvadratshi)
