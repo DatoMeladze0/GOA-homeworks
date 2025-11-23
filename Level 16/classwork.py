9e71a69f0d225b2b37a3a697c40ea4e85f768bd8
DatoMeladze0 <dmeladze6@gmail.com>
2025-11-23 21:41:06 +0400

    initial commit

---
+++ Level 16/classwork.py
@@ -0,0 +1,21 @@
+# # 1)
+
+# num = 100
+# while num <= 200:
+#     print(num)
+#     num += 1
+
+# # 2)
+
+# num1 = 0
+# while num1 != 20:
+#     print(num1)
+#     num1 += 2
+
+# # 3)
+
+# my_name = "Dato"
+# name = input()
+# while my_name != name:
+#     name = input()
+# print("This is my name")
---
+++ Level 16/homework.py
@@ -0,0 +1,22 @@
+# 1)
+
+# i = 10
+# while i >= -10:
+#     print(i)
+#     i -=1
+
+# 2)
+
+# for i in range(1, 101, 2):
+#     print(i)
+
+# 3)
+
+password = "goa123"
+attempts = 3
+user_password = input("Please enter your name (3 attempts): ")
+while user_password != password and attempts != 1:
+    print("Password is incorrect! Try again!")
+    attempts -= 1
+    print(str(attempts) + " attempt remaining.")
+    user_password = input()
