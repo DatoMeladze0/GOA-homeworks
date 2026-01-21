91eca4b54b294e2a45bb6f0c63ba890dc9d4f6ef
DatoMeladze0 <dmeladze6@gmail.com>
2026-01-21 15:25:13 +0400

    initial commit

---
+++ Level 29/classwork.py
@@ -0,0 +1,30 @@
+# 1)
+list  = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+print(list[0])
+
+# 2)
+print(list[-1])
+
+# 3)
+print(list[0 : 3])
+
+# 4)
+print(list[-2 :])
+
+# 5)
+print(list[1 : 8])
+
+# 6)
+print(list[: : -1])
+
+# 7)
+print(list[1])
+
+# 8)
+print(list[: : 2])
+
+# 9)
+print(list[4])
+
+# 10)
+print(list[0 : 4])
