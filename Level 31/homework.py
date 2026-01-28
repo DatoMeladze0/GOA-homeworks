8a2f60d236b290ba1e80a16b8814aba1a91ea995
DatoMeladze0 <dmeladze6@gmail.com>
2026-01-28 14:03:46 +0400

    initial commit

---
+++ Level 31/homework.py
@@ -0,0 +1,26 @@
+
+# 1)
+
+# .upper() - ს გადაჰყავს lowercase ასოები uppercase - ში. მაგალითად:
+print("football".upper())
+print("basketball".upper())
+print("volleyball".upper())
+# .lower() - ს გადაჰყავს uppercase ასოები lowercase - ში. მაგალითად:
+print("FOOTBALL".lower())
+print("BASKETBALL".lower())
+print("VOLLEYBALL".lower())
+# .capitalize() გადაიყვანს მთლიანი string - ის პირველ ასოს uppercase - ში და დანარჩენს lowercase - ში. მაგალითად:
+print("football".capitalize())
+print("basketball".capitalize())
+print("volleyball".capitalize())
+# .title() გადაიყვანს string - ის თითოეული სიტყვის პირველ ასოს uppercase - ში. მაგალითად:
+print("football is fun".title())
+print("basketball is fun".title())
+print("volleyball is fun".title())
+# .find() ეძებს მითითებული სიმბოლოს ინდექსის პოზიციას მოცემულ string - ში. მაგალითად:
+print("football".find("o"))
+print("basketball".find("t"))
+print("volleyball".find("b"))
+
+# 2)
+# ფუნქციებს, რომლების გამოძახებაც შეიძლება მხოლოდ ზოგიერთ ობიექტზე (string - ზე, სიებზე, და ა.შ.), ეწოდებათ dot . notation 
